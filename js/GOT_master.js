5ebb0da637abd5c15b631e43796eeb4f15e93f32
Chang <k_chang@tss-hr420-dm14.fc.ca>
2019-03-25 15:17:47 -0400

    round 1 adding data

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -10,7 +10,12 @@
 			lightBox 	= document.querySelector('.lightbox'),
 			video		= document.querySelector('video'),
 			closeLB 	= document.querySelector('.lightbox-close'),
-			banners		= document.querySelector("#houseImages");
+			banners		= document.querySelector('#houseImages'), 
+			houseName	= document.querySelector('.house-name')
+			houseInfo 	= document.querySelector('.house-info');
+
+	const houseData = [ // STARK 
+	`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`]
 
 	function showLightbox() {
 		//  grab the right video src
@@ -23,6 +28,13 @@
 		// start becomes Stark -> first make a capital S, then add ark (or any house name)
 		
 		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		// change the paragraph text 
+		// backticks are JS template strings
+		houseName.textContent = `House ${targetSrc}`;
+
+		// retrieves first index in array 
+		houseInfo.textContent = houseData[0];
 
 		video.src = `video/House-${targetSrc}.mp4`;
 
@@ -51,8 +63,8 @@
 		TweenMax.to(banners, 0.8, {right: totalOffset});
 	}
 
-	// shields.forEach(shield => shield.addEventListener('click', showLightbox));
-	shields.forEach(shield => shield.addEventListener('click', animateBanner));
+	shields.forEach(shield => shield.addEventListener('click', showLightbox));
+	// shields.forEach(shield => shield.addEventListener('click', animateBanner));
 
 	video.addEventListener('ended', hideLightbox);
 	closeLB.addEventListener('click', hideLightbox);
